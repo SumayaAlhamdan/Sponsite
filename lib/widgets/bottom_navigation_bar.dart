8030d209d6e4484401e3e6c2a2aa7e653ad37e5b
deemaalh <dddeema@gmail.com>
2023-10-04 16:39:57 +0300

    fixing view offers

--- lib/widgets/bottom_navigation_bar.dart
+++ lib/widgets/bottom_navigation_bar.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:sponsite/ViewCurrentSponsee.dart';
+import 'package:sponsite/screens/sponsee_screens/ViewCurrentSponsee.dart';
 import 'package:sponsite/screens/sponsee_screens/sponsee_home_screen.dart';
 
 class BottomNavBar extends StatefulWidget {
