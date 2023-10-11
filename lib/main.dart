bed23b83049c4d88ea72e5285c68a4e4523a02f2
deemaalh <dddeema@gmail.com>
2023-10-12 00:00:08 +0300

    main updates for notification
    still not fixed

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:googleapis/servicemanagement/v1.dart';
 import 'package:sponsite/screens/signIn_screen.dart';
 import 'package:sponsite/screens/splash_screen.dart';
 import 'package:sponsite/screens/sponsee_screens/ViewCurrentSponsee.dart';
@@ -15,7 +16,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   await FirebaseMessaging.instance.getInitialMessage();
-   FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  FirebaseMessaging.onBackgroundMessage( await _firebaseMessagingBackgroundHandler);
   runApp(MaterialApp(
     theme: ThemeData(useMaterial3: true),
     navigatorKey: navigatorKey,
@@ -53,25 +54,26 @@ void main() async {
   ));
 }
 Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
   final String clickAction = message.data['click_action'];
   final String notif_type = message.data['notif_type'];
+  print('im here deema handling');
 
-  if (clickAction == 'FLUTTER_NOTIFICATION_CLICK' && notif_type == 'status') {
-    final String offerId = message.data['offerId'];
-
-    // Use the navigator key to navigate to the specific page.
-    navigatorKey.currentState!.push(
-      MaterialPageRoute(
-        builder: (context) => ViewOffersSponsor(),
-      ),
-    );
-  }
-  else if(clickAction == 'FLUTTER_NOTIFICATION_CLICK' && notif_type == 'offer'){
-    navigatorKey.currentState!.push(
-      MaterialPageRoute(
-        builder: (context) => ViewCurrentSponsee(),
-      ),
-    );
-  }
+  if (clickAction == 'FLUTTER_NOTIFICATION_CLICK') {
+    if (notif_type == 'status') {
+       print('deema is updating a status');
+      navigatorKey.currentState!.push(
+        MaterialPageRoute(
+          builder: (context) => ViewOffersSponsor(),
+        ),
+      );
+    } else if (notif_type == 'offer') {
+      print('deema is sending an offer');
+      navigatorKey.currentState!.push(
+        MaterialPageRoute(
+          builder: (context) => ViewCurrentSponsee(),
+        ),
+      );
+    }
+    }
 }
-
